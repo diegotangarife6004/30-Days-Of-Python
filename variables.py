7c4ac1461d5196c3f868e4a268267b58d9ad80be
diegotangarife6004 <117369878+diegotangarife6004@users.noreply.github.com>
2023-02-28 15:11:36 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,45 @@
+# 'Day2 : 30 Days of python programming'
+first_name = 'Diego'
+last_name =  'Tangarife'
+full_name =  'Diego Tangarife'
+country = 'Colombia'
+city =    'Cali'
+age = 73
+year1 = 1950
+is_married = True
+is_true = 'yes'
+is_light_on = is_true
+profesion = str(15), hobbies = ['travel', 'fishing', 'reading']
+
+# Exercise No. 2
+# Check the data type of all variables
+type(first_name)
+type(last_name)
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year1)
+type(is_married)
+type(is_true)
+type(is_light_on)
+type(profesion)
+type(hobbies)
+
+len(first_name)
+len(last_name) 
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radius = 30
+area_of_circle = pi * (radius **2)
+cicum_of_circle = 2 * pi * radius
+radius1 = input('enter radius: ')
+area = pi *(radius1 ** 2)
+# I wiil Continue later this afternoon
